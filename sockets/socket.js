b36f5bc4521b96b6424dcf1a4a8b03f8c3e307d4
nanci <vepimal@yahoo.es>
2022-12-12 20:35:11 +0100

    Cambios sobre bandas terminados

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,21 @@
 const { io } = require('../index')
+const Bands = require('../models/bands');
+const Band =require('../models/band');
+
+const bands = new Bands();
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Heros del Silencio'));
+bands.addBand(new Band('Metalica'));
+bands.addBand(new Band('People'));
+
+console.log(bands);
+
 
 //Mensajes de sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => { console.log('Cliente desconectado') });
 
@@ -12,13 +25,36 @@ io.on('connection', client => {
 
         io.emit('mensaje', { admin: 'Nuevo Mensaje' });       
         
-    })
-    client.on('emitir-mensaje', (payload) => {
+    });
+
+    client.on('vote-band',(payload)=>{
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+
+    });
+
+    // Escuchar add-band 
+    client.on('add-band',(payload)=>{
+        bands.addBand(new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+
+    });
+
+
+    //delete-band
+    client.on('delete-band',(payload)=>{
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+
+    });
+
+
+
+
+  /*   client.on('emitir-mensaje', (payload) => {
         // console.log('Mensaje:', payload);
-
-
-       // io.emit('nuevo-mensaje',  payload );  //emite a todos   
+        // io.emit('nuevo-mensaje',  payload );  //emite a todos   
         client.broadcast.emit('nuevo-mensaje',  payload );  //emite a todos menos al que lo emitio   
         
-    })
+    }) */
 });
